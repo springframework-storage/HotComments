b69dcec7d845a53bdb0ecc841bf86c5405c1b835
codingsquid <gksxodnd007@gmail.com>
2018-05-13 18:33:40 +0900

    feat: 개발 환경 초기 설정
    브랜치로 나누어 개발하기위해 공통으로 사용될 기술들과 패키지들을 세팅하였습니다.
    temp.md는 패키지를 올리기위한 목적으로 넣은 임시파일이며 개발영역을 각각 나누게 되었으므로 인터페이스는 따로 맞추지 않았습니다.

---
+++ src/main/java/com/naver/hackday/HackdayApplication.java
@@ -0,0 +1,12 @@
+package com.naver.hackday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackdayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackdayApplication.class, args);
+	}
+}
---
+++ src/main/java/com/naver/hackday/config/RedisConfig.java
@@ -0,0 +1,73 @@
+package com.naver.hackday.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import redis.clients.jedis.JedisPoolConfig;
+
+import java.io.IOException;
+import java.util.Properties;
+
+@Configuration
+public class RedisConfig {
+
+    private JedisPoolConfig createJedisPoolConfig(final Properties redisConfig) {
+        final JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+
+        // set as default
+        jedisPoolConfig.setTestOnBorrow(false);
+        jedisPoolConfig.setTestOnReturn(false);
+        jedisPoolConfig.setTestWhileIdle(true);
+        jedisPoolConfig.setNumTestsPerEvictionRun(10);
+        jedisPoolConfig.setTimeBetweenEvictionRunsMillis(60000);
+        jedisPoolConfig.setMinIdle(1);
+
+        // set by config
+        jedisPoolConfig.setMaxWaitMillis(Long.parseLong(redisConfig.getProperty("redis.pool.maxWaitMillis")));
+        jedisPoolConfig.setMaxIdle(Integer.parseInt(redisConfig.getProperty("redis.pool.maxIdle")));
+        jedisPoolConfig.setMaxTotal(Integer.parseInt(redisConfig.getProperty("redis.pool.maxTotal")));
+
+        return jedisPoolConfig;
+    }
+
+    @Bean(name = "jedisConnectionFactory", destroyMethod = "destroy")
+    public JedisConnectionFactory jedisConnectionFactory() throws IOException {
+        final Properties redisConfig = PropertiesLoaderUtils.loadProperties(
+                new PathMatchingResourcePatternResolver().getResource("classpath:redis.properties"));
+
+        final JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory();
+
+        jedisConnectionFactory.setHostName(redisConfig.getProperty("redis.hostname"));
+        jedisConnectionFactory.setPassword(redisConfig.getProperty("redis.password"));
+        jedisConnectionFactory.setPort(Integer.parseInt(redisConfig.getProperty("redis.port")));
+        jedisConnectionFactory.setUsePool(Boolean.parseBoolean(redisConfig.getProperty("redis.pool.use")));
+        jedisConnectionFactory.setTimeout(Integer.parseInt(redisConfig.getProperty("redis.timeout")));
+        jedisConnectionFactory.setPoolConfig(createJedisPoolConfig(redisConfig));
+
+        jedisConnectionFactory.afterPropertiesSet();
+
+        return jedisConnectionFactory;
+    }
+
+    @Bean(name = "redisTemplate")
+    public RedisTemplate<String, Object> redisTemplate() throws IOException {
+        final RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+
+        redisTemplate.setConnectionFactory(jedisConnectionFactory());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
+
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
+
+        redisTemplate.afterPropertiesSet();
+
+        return redisTemplate;
+    }
+
+}
