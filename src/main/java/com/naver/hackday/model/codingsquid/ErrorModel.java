2dd50580b26b14839634822c62aa6a75a75fe5e0
codingsquid <gksxodnd007@gmail.com>
2018-05-15 10:08:44 +0900

    feat: exception 처리시 사용할 모델

---
+++ src/main/java/com/naver/hackday/model/codingsquid/ErrorModel.java
@@ -0,0 +1,18 @@
+package com.naver.hackday.model.codingsquid;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ErrorModel {
+
+    @JsonProperty("return_code")
+    private int errorCode;
+    @JsonProperty("return_message")
+    private String errorMessage;
+
+}
