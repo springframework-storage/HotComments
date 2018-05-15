5b843d310e703d8a0b470f1be6fc5ad69c5a95db
codingsquid <gksxodnd007@gmail.com>
2018-05-15 11:23:51 +0900

    feat: redis에서 캐싱된 데이터를 처리하기 위한 서비스

---
+++ src/main/java/com/naver/hackday/service/CommentCachingServiceImpl.java
@@ -0,0 +1,82 @@
+package com.naver.hackday.service;
+
+
+import com.naver.hackday.dto.CommentDto;
+import com.naver.hackday.dto.ValidCachingKey;
+import com.naver.hackday.model.codingsquid.BaseCode;
+import com.naver.hackday.model.codingsquid.BaseListRtn;
+import com.naver.hackday.model.codingsquid.BaseResponse;
+import com.naver.hackday.repository.cache.RedisRepository;
+import com.naver.hackday.util.cache.CachingKeyHelper;
+import com.naver.hackday.util.cache.CachingServiceHelper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Service
+public class CommentCachingServiceImpl implements CommentListService {
+
+    private final CommentListService commentListService;
+    private final RedisRepository redisRepository;
+    private final CachingServiceHelper<String, List<Object>> cachingServiceHelper;
+
+    @Autowired
+    public CommentCachingServiceImpl(@Qualifier("redisRepositoryImpl") RedisRepository redisRepository,
+                                     @Qualifier("commentOriginServiceImpl") CommentListService commentListService) {
+        this.redisRepository = redisRepository;
+        this.cachingServiceHelper = new CachingServiceHelper<>();
+        this.commentListService = commentListService;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public BaseResponse<BaseListRtn<CommentDto>> doGet(int cursor, int size, int pageNo, String orderType, int postId, int userId) {
+        BaseResponse<BaseListRtn<CommentDto>> result;
+        String cachingKey = CachingKeyHelper.getCommentListKey(Integer.toString(postId), Integer.toString(pageNo));
+        ValidCachingKey validKey = new ValidCachingKey();
+        validKey.setValidKey(cachingKey);
+
+        //TODO 캐싱이 필요한지 검사 && 캐싱 키 값 재설정 && 캐싱 로직 재설계 && 캐싱 만료시간 설정 && pipeline적용 && key가 존재하지 않을 경우 예외처리
+        if (cachingServiceHelper.isNeedCaching(
+                key -> redisRepository.isMember(key, validKey), //이 부분 수정해야함
+                CachingKeyHelper.getValidationCommentListKey())) {
+            result = commentListService.doGet(cursor, size, pageNo, orderType, postId, userId);
+            redisRepository.setListToListRight(cachingKey, result.getResult().getDatas());
+            for (CommentDto comments : result.getResult().getDatas()) {
+                redisRepository.setListToListRight(cachingKey, comments);
+            }
+            redisRepository.addMemberToSet(CachingKeyHelper.getValidationCommentListKey(), validKey);
+
+            Stream<CommentDto> rtnList = result.getResult().getDatas().stream();
+            List<CommentDto> dtos = rtnList.skip(cursor - 1)
+                    .limit(size)
+                    .collect(Collectors.toList());
+
+            result.getResult().setDatas(dtos);
+            result.getResult().setTotalSize(dtos.size());
+
+            return result;
+        }
+
+        //캐싱된 데이터를 반환
+        result = new BaseResponse<>();
+        BaseListRtn<CommentDto> commentDtoBaseListRtn = new BaseListRtn<>();
+
+        //TODO 데이터 크기 조정
+        List<Object> datas = cachingServiceHelper.useCaching(key -> redisRepository.getRangeFromList(key, cursor, (cursor + size - 1)), cachingKey);
+
+        commentDtoBaseListRtn.setDatas((List<CommentDto>)(Object) datas);
+        commentDtoBaseListRtn.setTotalSize(datas.size());
+
+        result.setResult(commentDtoBaseListRtn);
+        result.setReturnMessage(BaseCode.SUCCESS.getMessage());
+        result.setReturnCode(BaseCode.SUCCESS.getCode());
+
+        return result;
+    }
+
+}
