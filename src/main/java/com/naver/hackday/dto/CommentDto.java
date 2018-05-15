864e7cb448bd8d47a8f21b1d117a776c86d27d86
codingsquid <gksxodnd007@gmail.com>
2018-05-15 22:21:13 +0900

    feat: CommentDto @JsonProperty 삭제

--- src/main/java/com/naver/hackday/dto/CommentDto.java
+++ src/main/java/com/naver/hackday/dto/CommentDto.java
@@ -1,6 +1,5 @@
 package com.naver.hackday.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,19 +11,12 @@ import lombok.ToString;
 @EqualsAndHashCode
 public class CommentDto {
 
-    @JsonProperty("commentId")
     private int id;
-    @JsonProperty("postId")
     private int postId;
-    @JsonProperty("userId")
     private int userId;
-    @JsonProperty("content")
     private String content;
-    @JsonProperty("nCount")
     private int nCount;
-    @JsonProperty("pCount")
     private int pCount;
-    @JsonProperty("totalCount")
     private int totalCount;
 
 }
