9a97b930b39d45ad4b2aef66a8f37a8cfc852a5d
codingsquid <gksxodnd007@gmail.com>
2018-05-15 10:09:42 +0900

    feat: 요청에 대한 응답시 사용할 기본 모델

---
+++ src/main/java/com/naver/hackday/model/codingsquid/BaseResponse.java
@@ -0,0 +1,33 @@
+package com.naver.hackday.model.codingsquid;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+public class BaseResponse<T> {
+
+    public final static String RETURN_CODE = "return_code";
+    public final static String RETURN_MESSAGE = "return_message";
+    public final static String RESULT = "result";
+
+    @JsonProperty(RETURN_CODE)
+    private int returnCode;
+    @JsonProperty(RETURN_MESSAGE)
+    private String returnMessage;
+    @JsonProperty(RESULT)
+    private T result;
+
+    public BaseResponse() {}
+
+    public BaseResponse(int returnCode, String returnMessage) {
+        this.returnCode = returnCode;
+        this.returnMessage = returnMessage;
+    }
+
+}
