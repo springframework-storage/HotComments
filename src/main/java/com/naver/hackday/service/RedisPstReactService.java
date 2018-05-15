eb478fa7f0e2593b80b5c49e74906395d5cd3070
mingooddev <mingood92@gmail.com>
2018-05-15 20:30:23 +0900

    Feat: 공감 요청 처리 기능 service 구현 공감 처리 interface를 상속 받아 service를 구현합니다. Redis를 사용하기 위해 RedisTemplate Set구조 활용을 위해 SetOperation을 선언합니다. 추가적으로 공감 여부 확인을 위해 isMember 메소드를 추가합니다.

---
+++ src/main/java/com/naver/hackday/service/RedisPstReactService.java
@@ -0,0 +1,58 @@
+package com.naver.hackday.service;
+
+import com.naver.hackday.repository.PstReactRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SetOperations;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+
+@Service
+public class RedisPstReactService implements PstReactRepository {
+
+  private static final String KEY = "Pst";
+  private RedisTemplate<String, Integer> redisTemplate;
+  private SetOperations<String, Integer> setOperations;
+
+  @Autowired
+  private RedisPstReactService(RedisTemplate redisTemplate) {
+    this.redisTemplate = redisTemplate;
+  }
+
+  @PostConstruct
+  private void init() {
+    setOperations = redisTemplate.opsForSet();
+  }
+
+  // 사용자의 공감 요청 처리
+  @Override
+  public void pstReact(int postId, int commentId, int userId) {
+    // 이미 해당 댓글에 공감했다면 공감 취소
+    if (this.isMember(commentId, userId)) {
+      this.delete(commentId, userId);
+    }
+    // 아니라면 공감
+    else {
+      this.insert(postId, commentId, userId);
+    }
+  }
+
+  // Redis에 공감 데이터 삽입
+  @Override
+  public void insert(int postId, int commentId, int userId) {
+    this.setOperations.add(KEY + Integer.toString(commentId), userId);
+  }
+
+  // Redis에서 공감 데이터 삭제
+  @Override
+  public void delete(int commentId, int userId) {
+    this.setOperations.remove(KEY + Integer.toString(commentId), userId);
+  }
+
+  // 공감 여부 확인을 위한 메소드
+  public boolean isMember(int commentId, int userId) {
+    return this.setOperations.isMember(KEY + Integer.toString(commentId), userId);
+  }
+
+}
