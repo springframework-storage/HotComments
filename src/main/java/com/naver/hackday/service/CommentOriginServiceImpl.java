033a0c5bc452aec929b947f87954d7647d2815af
codingsquid <gksxodnd007@gmail.com>
2018-05-15 11:24:47 +0900

    feat: mysql에서 데이터를 조회하기위한 서비스

---
+++ src/main/java/com/naver/hackday/service/CommentOriginServiceImpl.java
@@ -0,0 +1,42 @@
+package com.naver.hackday.service;
+
+import com.naver.hackday.dto.CommentDto;
+import com.naver.hackday.model.codingsquid.BaseCode;
+import com.naver.hackday.model.codingsquid.BaseListRtn;
+import com.naver.hackday.model.codingsquid.BaseResponse;
+import com.naver.hackday.repository.origin.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CommentOriginServiceImpl implements CommentListService {
+
+    private CommentRepository commentRepository;
+
+    @Autowired
+    public CommentOriginServiceImpl(@Qualifier("commentRepositoryImpl") CommentRepository commentRepository) {
+        this.commentRepository = commentRepository;
+    }
+
+    @Override
+    public BaseResponse<BaseListRtn<CommentDto>> doGet(int cursor, int size, int pageNo, String orderType, int postId, int userId) {
+        BaseResponse<BaseListRtn<CommentDto>> result = new BaseResponse<>();
+        BaseListRtn<CommentDto> baseListRtn = new BaseListRtn<>();
+
+        //캐싱 히트율을 높이기위해 페이지별로 데이터를 100개씩 가져옴.
+        List<CommentDto> dtos = commentRepository.retreiveCommentOrderByLikeCount((pageNo - 1) * 100, 100);
+
+        baseListRtn.setTotalSize(100);
+        baseListRtn.setDatas(dtos);
+
+        result.setReturnCode(BaseCode.SUCCESS.getCode());
+        result.setReturnMessage(BaseCode.SUCCESS.getMessage());
+        result.setResult(baseListRtn);
+
+        return result;
+    }
+
+}
