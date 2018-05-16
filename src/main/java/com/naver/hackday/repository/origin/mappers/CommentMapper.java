ac5a667ea44db61d9645e5923c9692a32f3f9784
jaehee <akwogml112@gmail.com>
2018-05-17 00:45:23 +0900

    feat: CommentMapper interface에 메소드 추가
    공감 취소 요청에 따라 MySQL의 공감 수치를 감소 시키는 메소드를 추가

--- src/main/java/com/naver/hackday/repository/origin/mappers/CommentMapper.java
+++ src/main/java/com/naver/hackday/repository/origin/mappers/CommentMapper.java
@@ -5,4 +5,5 @@ import java.util.List;
 public interface CommentMapper {
 	//각 댓글의 pCount를 증가시킴
 	void updateIncreasePCount(List<? extends Integer> commentIdList);
+	void updateDecreasePCount(List<? extends Integer> commentIdList);
 }
