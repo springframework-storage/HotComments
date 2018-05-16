6b41f0e8c754240b18ae7f773379096afc3b1adf
jaehee <akwogml112@gmail.com>
2018-05-17 00:29:05 +0900

    feat: CommentMapper 생성
    댓글 id 리스트를 파라미터로 받아 각 댓글의 pCount를 증가시키기 위한 updateIncreasePCount메소드 생성

---
+++ src/main/java/com/naver/hackday/repository/origin/mappers/CommentMapper.java
@@ -0,0 +1,8 @@
+package com.naver.hackday.repository.origin.mappers;
+
+import java.util.List;
+
+public interface CommentMapper {
+	//각 댓글의 pCount를 증가시킴
+	void updateIncreasePCount(List<? extends Integer> commentIdList);
+}
