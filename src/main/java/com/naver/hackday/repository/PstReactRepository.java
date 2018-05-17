8c19bc95a47e5ca9fee9599d2cb2a42f077e918b
mingooddev <mingood92@gmail.com>
2018-05-17 23:19:29 +0900

    fixup! Feat: 공감 요청 처리 기능 interface 구현 공감 요청 처리에 공통적으로 사용될 메소드라고 생각되는 것들을 interface 안에 선언합니다.

--- src/main/java/com/naver/hackday/repository/PstReactRepository.java
+++ src/main/java/com/naver/hackday/repository/PstReactRepository.java
@@ -3,7 +3,5 @@ package com.naver.hackday.repository;
 public interface PstReactRepository {
 
   void pstReact(int postId, int commentId, int userId);
-  void insert(int postId, int commentId, int userId);
-  void delete(int commentId, int userId);
 
 }
