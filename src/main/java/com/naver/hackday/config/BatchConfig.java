9e56a8d9fe4a451888354973dbea96b5a397bb56
jaehee <akwogml112@gmail.com>
2018-05-17 15:46:18 +0900

    feat: BatchConfig 수정
    공감 수치를 증가시키는 Job과 감소시키는 Job을 비동기로 처리하기 위하여 TaskExecutor를 추가.
    동기적으로 수행하였을 때의 지연시간을 제거하는 효과를 얻을 수 있음.

--- src/main/java/com/naver/hackday/config/BatchConfig.java
+++ src/main/java/com/naver/hackday/config/BatchConfig.java
@@ -5,70 +5,77 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.support.SimpleJobLauncher;
+import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import com.naver.hackday.step.DecreaseReader;
 import com.naver.hackday.step.DecreaseWriter;
 import com.naver.hackday.step.IncreaseReader;
 import com.naver.hackday.step.IncreaseWriter;
 
-
 @Configuration
 @EnableBatchProcessing
 public class BatchConfig {
-	
-	@Autowired 
+
+	@Autowired
 	public JobBuilderFactory jobBuilderFactory;
-	
+
 	@Autowired
 	public StepBuilderFactory stepBuilderFactory;
-	
+
 	@Autowired
 	private DecreaseReader decreaseReader;
-	
+
 	@Autowired
 	private DecreaseWriter decreaseWriter;
-	
+
 	@Autowired
 	private IncreaseReader increaseReader;
-	
+
 	@Autowired
 	private IncreaseWriter increaseWriter;
-	
+
 	@Bean
 	public Job increaseEmpathyJob() {
-		return jobBuilderFactory.get("increaseEmpathyJob")
-				.flow(increaseEmpathyStep())
-				.end()
-				.build();
+		return jobBuilderFactory.get("increaseEmpathyJob").flow(increaseEmpathyStep()).end().build();
 	}
-	
+
 	@Bean
 	public Step increaseEmpathyStep() {
-		return stepBuilderFactory.get("increaseEmpathyStep")
-				.<Integer, Integer>chunk(3000)
-				.reader(increaseReader)
-				.writer(increaseWriter)
-				.build();
+		return stepBuilderFactory.get("increaseEmpathyStep").<Integer, Integer>chunk(3000).reader(increaseReader)
+				.writer(increaseWriter).build();
 	}
-	
+
 	@Bean
 	public Job decreaseEmpathyJob() {
-		return jobBuilderFactory.get("decreaseEmpathyJob")
-				.flow(decreaseEmpathyStep())
-				.end()
-				.build();
+		return jobBuilderFactory.get("decreaseEmpathyJob").flow(decreaseEmpathyStep()).end().build();
 	}
 
 	@Bean
 	public Step decreaseEmpathyStep() {
-		return stepBuilderFactory.get("decreaseEmpathyStep")
-				.<Integer, Integer>chunk(3000)
-				.reader(decreaseReader)
-				.writer(decreaseWriter)
-				.build();
+		return stepBuilderFactory.get("decreaseEmpathyStep").<Integer, Integer>chunk(3000).reader(decreaseReader)
+				.writer(decreaseWriter).build();
+	}
+
+	@Bean
+	public ThreadPoolTaskExecutor taskExecutor() {
+		ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+		taskExecutor.setCorePoolSize(15);
+		taskExecutor.setMaxPoolSize(20);
+		taskExecutor.setQueueCapacity(30);
+		return taskExecutor;
+	}
+
+	@Bean
+	public JobLauncher jobLauncher(ThreadPoolTaskExecutor taskExecutor, JobRepository jobRepository) {
+		SimpleJobLauncher jobLauncher = new SimpleJobLauncher();
+		jobLauncher.setTaskExecutor(taskExecutor);
+		jobLauncher.setJobRepository(jobRepository);
+		return jobLauncher;
 	}
 }
-
