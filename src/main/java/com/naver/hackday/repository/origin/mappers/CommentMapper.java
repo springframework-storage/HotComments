8360855fff5eafa924796a351676d16f184baa8e
jaehee <akwogml112@gmail.com>
2018-05-17 21:21:51 +0900

    feat: Mapper의 Method명 변경
    뜻은 같으나 문법을 맞추며 간략화함.

--- src/main/java/com/naver/hackday/repository/origin/mappers/CommentMapper.java
+++ src/main/java/com/naver/hackday/repository/origin/mappers/CommentMapper.java
@@ -4,6 +4,6 @@ import java.util.List;
 
 public interface CommentMapper {
 	//각 댓글의 pCount를 증가시킴
-	void updateIncreasePCount(List<? extends Integer> commentIdList);
-	void updateDecreasePCount(List<? extends Integer> commentIdList);
+	void increasePCount(List<? extends Integer> commentIdList);
+	void decreasePCount(List<? extends Integer> commentIdList);
 }
