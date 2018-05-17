44386a4879d317a29846adaf2a7cb179b9398150
mingooddev <mingood92@gmail.com>
2018-05-18 00:02:44 +0900

    fixup! fixup! Feat: 공감 요청을 받을 ReactController 구현 parameter - postId, commentId, userId, react 파라미터로 받은 commentId에 해당하는 Comment를 찾아 존재한다면 그 Comment의 Id를 service의 parameter로 넘깁니다.

--- src/main/java/com/naver/hackday/controller/ReactController.java
+++ src/main/java/com/naver/hackday/controller/ReactController.java
@@ -2,6 +2,7 @@ package com.naver.hackday.controller;
 
 import com.naver.hackday.dto.CommentDto;
 import com.naver.hackday.model.DefaultResponse;
+import com.naver.hackday.model.ReactParam;
 import com.naver.hackday.model.Status;
 import com.naver.hackday.service.CommentService;
 import com.naver.hackday.service.ReactCheckService;
@@ -23,17 +24,29 @@ public class ReactController {
   @Autowired
   private ReactCheckService reactCheckService;
 
-  @GetMapping("{postId}/{commentId}/{userId}/{react}")
+  /**
+   * 사용자의 공감 요청을 받습니다.
+   * @param postId
+   * @param commentId
+   * @param userId
+   * @param reactParam : Enum(Pst,Ngt)로 제약이 있도록 합니다.
+   * @return
+   */
+  @GetMapping("{postId}/{commentId}/{userId}/{reactParam}")
   public ResponseEntity<DefaultResponse> integrationReact(@PathVariable int postId,
                                                           @PathVariable int commentId,
                                                           @PathVariable int userId,
-                                                          @PathVariable String react) {
+                                                          @PathVariable ReactParam reactParam) {
     DefaultResponse res = new DefaultResponse();
 
     // 해당 commentId의 댓글이 존재하는지 찾습니다.
     CommentDto comment = commentService.findById(commentId);
 
-    reactCheckService.reactCheck(postId, comment.getId(), userId, react);
+    /*
+    CheckService로 파라미터를 넘길 때
+    reactParam의 Value를 꺼내 int로 변환합니다.
+     */
+    reactCheckService.reactCheck(postId, comment.getId(), userId, reactParam.getValue());
 
     /*
     Comment 자체를 넘김으로서
