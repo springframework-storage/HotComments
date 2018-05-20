395e4b720712213a4ec52eb4db0c03a4f284bdcc
mingooddev <mingood92@gmail.com>
2018-05-20 20:54:14 +0900

    Test: ReactController Test 코드 추가

---
+++ src/test/java/com/naver/hackday/controller/ReactControllerTest.java
@@ -0,0 +1,41 @@
+package com.naver.hackday.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.handler;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReactControllerTest {
+
+  private MockMvc mockMvc;
+
+  @Autowired
+  private WebApplicationContext webApplicationContext;
+
+  @Before
+  public void setUp() throws Exception {
+    this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
+  }
+
+  @Test
+  public void integrationReact() throws Exception {
+    this.mockMvc.perform(get("/react/1/7/3/Pst"))
+                .andExpect(handler().handlerType(ReactController.class))
+                .andExpect(handler().methodName("integrationReact"))
+                .andExpect(status().isOk())
+                .andDo(MockMvcResultHandlers.print());
+  }
+
+}
