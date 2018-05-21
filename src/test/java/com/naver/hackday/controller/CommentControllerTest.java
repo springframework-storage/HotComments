560d682eee63394fcb4aec2e4cede5c801fc42b0
mingooddev <mingood92@gmail.com>
2018-05-21 18:01:31 +0900

    Test: (React, Comment)Controller @Test 코드 추가 및 수정

--- src/test/java/com/naver/hackday/controller/CommentControllerTest.java
+++ src/test/java/com/naver/hackday/controller/CommentControllerTest.java
@@ -1,26 +1,37 @@
 package com.naver.hackday.controller;
 
+import com.naver.hackday.dto.CommentDto;
+import com.naver.hackday.model.codingsquid.BaseListRtn;
+import com.naver.hackday.model.codingsquid.BaseResponse;
+import com.naver.hackday.service.CommentOriginServiceImpl;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.assertj.core.api.Java6Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.handler;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ReactControllerTest {
+@AutoConfigureMockMvc
+@Slf4j
+public class CommentControllerTest {
 
   private MockMvc mockMvc;
+
+  @Autowired
+  private CommentOriginServiceImpl commentOriginService;
 
   @Autowired
   private WebApplicationContext webApplicationContext;
@@ -31,14 +42,21 @@ public class ReactControllerTest {
   }
 
   @Test
-  public void integrationReact() throws Exception {
-    this.mockMvc.perform(get("/react/1/7/3/Pst")
-                .accept(MediaType.APPLICATION_JSON)
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(handler().handlerType(ReactController.class))
-                .andExpect(handler().methodName("integrationReact"))
+  public void getCommentList() throws Exception {
+    this.mockMvc.perform(get("/v1/comments/1")
+                .param("userId", "1")
+                .param("cursor", "1")
+                .param("pageSize", "100")
+                .param("orderType", "ASC")
+                .param("pageNo", "1"))
+                .andExpect(handler().handlerType(CommentController.class))
+                .andExpect(handler().methodName("doGet"))
                 .andExpect(status().isOk())
                 .andDo(MockMvcResultHandlers.print());
+
+    // 위 코드와 별개 - 간이 테스트
+    BaseResponse<BaseListRtn<CommentDto>> result = this.commentOriginService.doGet(1, 1, 100, "ASC", 1, 1);
+    assertThat(result).isNotNull();
   }
 
 }
